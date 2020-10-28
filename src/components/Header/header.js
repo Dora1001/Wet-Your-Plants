95d485ea7c153148d4951a8be7cd697bc56faceb
Dorota Janus <dot@MacBook-Pro-Dorota.local>
2020-10-28 17:04:58 +0100

    header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -4,7 +4,19 @@ import "./header.scss";
 export const Header = () => {
     return (
         <header className="App-header">
-            <h1>Hello World!</h1>
+            <div className="App-logo">
+
+            <img className="App-logo-img" src="../../images/logo_HOP.svg"/>
+            </div>
+
+            <div className="App-slider">
+                <div className="App-slider-txt">
+                    <h1>Filodendrony</h1>
+                    <p>Odkryj świat filodendronów</p>
+                </div>
+                <img className="App-slider-img" src="../../images/slider1.jpg"/>
+            </div>
+
         </header>
     );
 }
